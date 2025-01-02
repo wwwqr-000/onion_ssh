0fe2daf29266879f05729c68324a1b4ebca112a7
wwwqr-000 <wwwqrminecraft@gmail.com>
2025-01-02 19:31:09 +0100

    Almost done, integrating config.json

--- windows/onion_ssh/main.cpp
+++ windows/onion_ssh/main.cpp
@@ -2,13 +2,17 @@
 #include <windows.h>
 #include <vector>
 #include <thread>
+#include <fstream>
+
+#include "classes/json.hpp"
+using json = nlohmann::json;
 
 std::vector<std::thread> threads;
 bool intentionalShutdown = false;
-
 HMODULE const DLL = LoadLibraryExW(L"data/whiteavocado64.dll", nullptr, 0);
-
 std::string dataFiles[2] = {"data/tor-whiteavocado.exe", "data/config.json"};
+json configData;
+std::string title = "whiteavocado onion ssh";
 
 #include "classes/dll_methods.hpp"
 
@@ -31,7 +35,7 @@ void torInstance() {
     std::string buff;//Buffer that contains the result of the quietShell instance.
     quietShell(("start " + dataFiles[0]).c_str(), buff);//Instance
     if (intentionalShutdown) { return; }
-    msgBox("whiteavocado onion ssh", "tor instance stopped or already running, ignoring.", "o", "w", buff);
+    msgBox(title.c_str(), "tor instance stopped or already running, ignoring.", "o", "w", buff);
 }
 
 void startTor() {
@@ -52,21 +56,48 @@ void stopTor() {
     quietShell("taskkill /IM tor-whiteavocado.exe /F", buff);
 }
 
+void setConfig() {
+    std::ifstream f(dataFiles[1]);
+    configData = json::parse(f);
+    std::string buff = "";
+    try {
+        buff = configData["torService"]["port"];
+        buff = configData["torService"]["ip"];
+        buff = configData["torService"]["socksVersion"];
+        buff = configData["firstUse"];
+        buff = "";
+    }
+    catch (std::exception) {
+        msgBox(title.c_str(), (dataFiles[1] + " file is not a valid format or is missing required indexes.").c_str(), "o", "e", buff);
+        exit(1);
+    }
+    if (configData["firstUse"] == "true") {
+        configData["firstUse"] = "false";
+        msgBox(title.c_str(), "Thanks for downloading the onion ssh tool from whiteavocado!\n\nDependencies for this tool:\n* putty (plink)\n* netcat\n\nWould you like to go to the download page of netcat and putty?", "yn", "i", buff);
+        if (buff == "yes") {
+            system("start https://www.putty.org/ && start https://nmap.org/ncat/");
+            buff = "";
+            msgBox(title.c_str(), ("Would you like to start " + title + "?").c_str(), "yn", "q", buff);
+            if (buff != "yes") { exit(0); }
+        }
+    }
+}
+
 int main() {
-    system("@echo off && title whiteavocado onion ssh && color a && cls");
+    system(("@echo off && title " + title + " && color a && cls").c_str());
     if (!dependenciesExist()) {
         system("color 4");
         std::cout << "depending file(s) do not exist or no access. Aborted.\n\nPress enter to exit\n";
         wait();
-        return 1;
+        return 2;
     }
+    setConfig();
     startTor();
     std::string connStr = "";
-    std::cout << "whiteavocado onion ssh\n\n>ssh ";
+    std::cout << title << "\n\n>ssh ";
     std::cin >> connStr;
-    system(("plink -ssh -proxycmd \"ncat --proxy 127.0.0.1:9050 --proxy-type socks5 %host %port\" " + connStr).c_str());
+    system(("plink -ssh -proxycmd \"ncat --proxy " + configData["torService"]["ip"] + ":" + configData["torService"]["port"] + " --proxy-type socks" + configData["torService"]["socksVersion"] + " %host %port\" " + connStr).c_str());
     stopTor();
     killThreads();
-
     return 0;
 }
